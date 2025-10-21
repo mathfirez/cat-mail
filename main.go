549504f15ea8d6ac55852306379a29214c69f6b6
mathfirez <mncollacique@gmail.com>
2025-10-20 21:19:22 -0300

    fix: removing scheduler functionality
    moving to client

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"cat-mail/src/config"
-	"cat-mail/src/processor"
 	"cat-mail/src/router"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 )
 
 func main() {
@@ -18,17 +15,6 @@ func main() {
 	fmt.Println("Creating routes...")
 	r := router.CreateRoutes()
 	fmt.Println("Routes created!")
-
-	// Gets Scheduler interval.
-	interval, err := strconv.Atoi(os.Getenv("INTERVAL"))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// TODO: will be deprecated, remove from here.
-	// Starts scheduler service.
-	go processor.Scheduler(interval)
 
 	fmt.Printf("Listening on port: %d", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
--- src/processor/processor.go
+++ src/processor/processor.go
@@ -3,26 +3,10 @@ package processor
 import (
 	"cat-mail/src/models"
 	"fmt"
-	"time"
 )
 
 // Adds a message to the queue (db).
 // TODO
 func AddToQueue(message models.Message) {
 	fmt.Println(message)
-}
-
-// TODO: Deprecate here, will be moved to the client.
-// Queries the database for messages and sends to the printer queue based on the interval defined in the .env file.
-// Sends a single message per run to avoid bloating.
-func Scheduler(interval int) {
-	seconds := time.Duration(interval) * time.Second
-	for {
-		time.Sleep(seconds)
-		//Querying new messages
-
-		// If no messages, sleep again
-		// Else, sends to printer, update message fields on DB based (see models.Message)
-	}
-
 }
